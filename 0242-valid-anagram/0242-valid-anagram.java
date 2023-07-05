8aab021d53678011275554c24e9f5bf46542e966
Amaan <amaan14999@gmail.com>
2023-07-06 01:19:45 +0530

    Time: 4 ms (85.78%), Space: 42 MB (91.69%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length())
+        return false;
+
+        int[] ans = new int[26];
+        for(int i = 0; i < s.length(); i++)
+        ans[s.charAt(i)-'a']++;
+
+        for(int i = 0; i < t.length(); i++)
+        ans[t.charAt(i)-'a']--;
+
+        for(int i : ans){
+            if(i!=0)
+            return false;
+        }
+        return true;
+    }
+}
