bfeedc34f0513448409c378ff4fe44de2aa0929c
Amaan <amaan14999@gmail.com>
2023-09-24 01:27:45 +0530

    Time: 3 ms (43.69%), Space: 56.4 MB (21.48%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.java
+++ 0932-monotonic-array/0932-monotonic-array.java
@@ -1,19 +1,35 @@
 class Solution {
+    // public boolean isMonotonic(int[] nums) {
+    //    return isInc(nums) || isDec(nums); 
+    // }
+    // public boolean isInc(int[] nums){
+    //     for(int i = 1; i < nums.length; i++){
+    //         if(nums[i] < nums[i - 1])
+    //         return false;
+    //     }
+    //     return true;
+    // }
+    // public boolean isDec(int[] nums){
+    //     for(int i = 1; i < nums.length; i++){
+    //         if(nums[i] > nums[i - 1])
+    //         return false;
+    //     }
+    //     return true;
+    // }
+
     public boolean isMonotonic(int[] nums) {
-       return isInc(nums) || isDec(nums); 
-    }
-    public boolean isInc(int[] nums){
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i] < nums[i - 1])
-            return false;
+        boolean increasing = true;
+        boolean decreasing = true;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                increasing = false;
+            }
+            if (nums[i] < nums[i + 1]) {
+                decreasing = false;
+            }
         }
-        return true;
-    }
-    public boolean isDec(int[] nums){
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i] > nums[i - 1])
-            return false;
-        }
-        return true;
+
+        return increasing || decreasing;
     }
 }
